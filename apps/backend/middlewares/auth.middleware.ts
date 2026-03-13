ccfc121af9cae05cab627b91e836c75990cc2497
SahilShangloo <sahilshangloo35@gmail.com>
2026-03-13 06:59:45 +0000

    Frontend Init

---
+++ apps/backend/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const header = req.headers.authorization;
+  const token = header?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Authorization Failed",
+    });
+  }
+
+  try {
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+
+    if (userId) {
+      req.userId = userId;
+    }
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Authorization Failed",
+    });
+  }
+};
