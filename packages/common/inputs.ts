fd1197adfb70c5216d06f7ad9e1770516bab17ae
SahilShangloo <sahilshangloo35@gmail.com>
2026-03-17 08:47:00 +0000

    Add common exports

--- packages/common/inputs.ts
+++ packages/common/inputs.ts
@@ -1,6 +1,17 @@
-import z from "zod";
+import z, { email, object } from "zod";
 
 export const SigninSchema = z.object({
   email: z.string(),
   password: z.string(),
+});
+
+export const CreateUserSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+  phone: z.string(),
+});
+
+export const SendSchema = z.object({
+  to: z.string(),
+  amount: z.number(),
 });
